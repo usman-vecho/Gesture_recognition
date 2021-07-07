fd467c8e311d45219cf79d5cc02c2368bc223eba
usman-vecho <86795519+usman-vecho@users.noreply.github.com>
2021-07-07 18:40:32 +0500

    Update gest_recon.py

--- gest_recon.py
+++ gest_recon.py
@@ -1,50 +1,62 @@
 import streamlit as st
 import cv2,pyautogui
 from cvzone.HandTrackingModule import HandDetector
+from win32api import GetSystemMetrics
+import sys,os
 
-st.title('Project - Arabic Sign Language')
 
-cap = cv2.VideoCapture(0)
+'''
+In order to close game, Display left hand smallest finger to camera
+'''
+
+
+
 frameST = st.empty()
 croppedST = st.empty()
 label = st.empty()
 
-cap.set(3, 1280)
-cap.set(4, 720)
-detector = HandDetector(detectionCon=0.5, maxHands=1)
+st.title('Hand gesture recognition- Lets Play with just hands')
 
+def exit():
+    pass
+    #os.system("taskkill /f /im  python.exe")
 
+if True:
+    width = GetSystemMetrics(0)
+    height = GetSystemMetrics(1)
+    
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1280)
+    cap.set(4, 720)
+    detector = HandDetector(detectionCon=0.5, maxHands=1)
 
+    while True:
+        success, img = cap.read()
+        img = cv2.flip(img,1)
+        img = detector.findHands(img)
 
-while True:
+        lmList, bboxInfo = detector.findPosition(img)
+        if lmList:
+            bbox = bboxInfo['bbox']
+            fingers = detector.fingersUp()
+            print(fingers)
+            if fingers==[0,0,0,0,1]:
+                exit()
 
-    success, img = cap.read()
+            totalFingers = fingers.count(1)
+            if int(totalFingers)==4 or int(totalFingers)==5:
+                (x, y) = pyautogui.position()
+                mov_x = 0.9375*width
+                mov_y = 0.8240*height
+                pyautogui.click(mov_x, mov_y)
+                pyautogui.moveTo(x, y)
 
+        frameST.image(img, channels="BGR")
+        #cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
-    img = detector.findHands(img)
-    lmList, bboxInfo = detector.findPosition(img)
-    if lmList:
-        bbox = bboxInfo['bbox']
-
-
-        fingers = detector.fingersUp()
-        totalFingers = fingers.count(1)
-        if int(totalFingers)==4 or int(totalFingers)==5:
-            (x, y) = pyautogui.position()
-
-            pyautogui.click(1800, 890)
-
-            pyautogui.moveTo(x, y)
-        #cv2.putText(img, f'Fingers:{totalFingers}', (bbox[0] + 200, bbox[1] - 30),cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
-
-    img2 = img.copy()
-    img2 = cv2.resize(img2,(200,100))
-    #cv2.imshow("Image", img2)
-    frameST.image(img, channels="BGR")
-    #label.info(f'Fingers:{totalFingers}')
-    #if cv2.waitKey(1) & 0xFF == ord('q'):
-    #    break
-#cap.release()
-#cv2.destroyAllWindows()
 
 
